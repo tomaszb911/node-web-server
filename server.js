5b46ca5d39b733a8296c0a4c54eeb6e70500f96b
burdka <tomasz.burdka@tomtom.com>
2019-01-29 16:52:35 +0100

    adding .gitignore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
@@ -7,14 +8,27 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
 
 app.use((req, res, next) => {
     var now = new Date().toString();
 
-    console.log(`${now}: ${req.method} ${req.url}`);
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to appedn to server.log');
+        }
+    });
     next();
 });
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
