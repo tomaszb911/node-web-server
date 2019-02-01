6e18b65fae73369cd9ca6643a3967563305fab40
tomaszb911 <burtek303@gmail.com>
2019-02-01 19:36:16 +0100

    adding projects page

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+});
+
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'uups, something went wrong :('
