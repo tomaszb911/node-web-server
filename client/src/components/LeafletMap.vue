ea9b035d52b935254fdc91e5aa6af0b354ff3ba0
burdka <tomasz.burdka@tomtom.com>
2019-03-17 22:34:51 +0100

    first try with vue in this project

--- client/src/components/LeafletMap.vue
+++ client/src/components/LeafletMap.vue
@@ -1,29 +1,98 @@
+// app.vue
 <template>
-  <div>ssssssss</div>
+  <div id="app">
+    <div id="map"></div>
+  </div>
 </template>
 
 <script>
+import L from "leaflet";
 export default {
-  name: 'Map',
-  props: {
-    msg: String
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      var coolPlaces = new L.LayerGroup();
+
+      L.marker([51.60127866491234, 5.335532247876811])
+        .bindPopup("JTS")
+        .addTo(coolPlaces);
+      L.marker([51.60133993313941, 5.335471914203235])
+        .bindPopup("TRUF")
+        .addTo(coolPlaces);
+
+      var pubLatLng = L.latLng(51.75723, -1.260269);
+
+      var ttMap = L.tileLayer(
+        "https://{s}.api.tomtom.com/map/1/tile/basic/main/{z}/{x}/{y}.png?key=EhMQIGSI8EE41do0KNiXKGaPjegSIg6V"
+      );
+      var osmLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>',
+        thunLink = '<a href="http://thunderforest.com/">Thunderforest</a>';
+
+      var osmUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        osmAttrib = "&copy; " + osmLink + " Contributors",
+        landUrl = "http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png",
+        thunAttrib = "&copy; " + osmLink + " Contributors &" + thunLink;
+      var osmMap = L.tileLayer(osmUrl, {
+        attribution: osmAttrib
+      });
+
+      var myMap = L.map("map", {
+        center: [52.38377, 4.75459],
+        zoom: 18,
+        layers: [osmMap, ttMap]
+      });
+
+      var baseLayers = {
+        "General View": osmMap,
+        "TT base map": ttMap
+      };
+      var overlays = {
+        "Places visited": coolPlaces
+      };
+      var pubMarker = L.marker(pubLatLng)
+        .bindPopup("hey");
+
+      (pubMarker).addTo(myMap);
+      L.control.layers(baseLayers, overlays).addTo(myMap);
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+
+
+<style>
+#app,
+#mymap {
+  position: relative;
+  padding: 0;
+  width: 600px;
+  height: 600px;
 }
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
