8a775c591309ddc61e700f97481e9e19b40985dc
tomaszb911 <burtek303@gmail.com>
2019-04-22 18:59:58 +0200

    registered components, but not working well vue-leaflet

--- client/src/components/LeafletMap.vue
+++ client/src/components/LeafletMap.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>ssssssss</div>
+    <div style="height: 100%">
+  <v-map :zoom=13 :center="[47.413220, -1.219482]">
+    <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+    <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
+  </v-map>
+</div>
 </template>
 
 <script>
+import * as Vue2Leaflet from 'vue2-leaflet';
+import Vue from 'vue';
+// import  L from 'leaflet';
+// new Vue({
+//   el: 'map',
+//   components: {
+//     'v-map': Vue2Leaflet.LMap,
+//         'v-tilelayer' :Vue2Leaflet.LTileLayer,
+//         'v-marker': Vue2Leaflet.LMarker,
+//     L },
+//   data() {
+//     return {
+//       zoom:13,
+//       center: L.latLng(47.413220, -1.219482),
+//       url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+//       attribution :'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+//       marker: L.latLng(47.413220, -1.219482),
+//     }
+//   }
+// });
+
 export default {
-  name: 'Map',
+  name: 'leafletMap',
+    components: {
+      
+    'v-map': Vue2Leaflet.LMap,
+        'v-tilelayer' :Vue2Leaflet.LTileLayer,
+        'v-marker': Vue2Leaflet.LMarker
+    },
   props: {
     msg: String
-  }
+  },
+//    
 }
 </script>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import L from 'leaflet';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import * as Vue2Leaflet from 'vue2-leaflet';
+// import LeafletMap from './components/LeafletMap';
+// Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(L);
+Vue.use(Vue2Leaflet);
+const router = new VueRouter({
+  routes: [{
+      path: '/',
+      name: 'app',
+      component: App
+  } ,]
+});
 
+Vue.router - router;
+App.router = Vue.router;
 new Vue({
+  router,
+  L,
   render: h => h(App),
 }).$mount('#app')
